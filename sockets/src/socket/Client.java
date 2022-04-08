abee34a5a6cb54f97a6562f20f05dda50fe4d667
Icaro Peretti <icaroperetti50@gmail.com>
2022-04-07 23:45:01 -0300

    Transmiting a arrayList through a socket

--- sockets/src/socket/Client.java
+++ sockets/src/socket/Client.java
@@ -1,12 +1,31 @@
 package socket;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         int port = 8000;
         String host = "127.0.0.1";
         Socket socket = new Socket(host, port);
+
+
+
+
+        ArrayList<String> messages = new ArrayList<String>();
+
+        messages.add("Icaro");
+        messages.add("Rafaels");
+        messages.add("Eduardo, velotax não tem como esquece");
+
+
+        OutputStream output = socket.getOutputStream();
+
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(output);
+
+        objectOutputStream.writeObject(messages);
+
+        socket.close();
     }
 }

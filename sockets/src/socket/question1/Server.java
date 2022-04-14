daf8d18acf36fa46d8bd0c125099155b1edb67fe
Icaro Peretti <icaroperetti50@gmail.com>
2022-04-14 08:24:12 -0300

    Add files via upload

--- sockets/src/socket/question1/Server.java
+++ sockets/src/socket/question1/Server.java
@@ -5,6 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 
 
 public class Server {
@@ -19,20 +20,21 @@ public class Server {
             lastVal = element;
         }else{
             System.out.println("Lista não encontrada!\n");
-            return ;
         }
     }
 
     public  static void removeElement(String listName,String element){
         ArrayList<String> list = lists.get(listName);
+        System.out.println(list);
         if(list != null){
-            lists.get(listName).add(element);
+            lists.get(listName).remove(element);
             lastVal = element;
         }else{
             System.out.println("Lista não encontrada!\n");
             return ;
         }
     }
+
 
     public static String getAllLists(){
         StringBuilder val = new StringBuilder();
@@ -59,11 +61,11 @@ public class Server {
                 + "3) Criar nova lista\n"
                 + "4) Imprimir último elemento adicionado\n"
                 + "5) Remover elemento de uma lista\n"
-                + "6) Fechar conexão\n";
+                + "6) Encerrar conexão\n";
         return menu;
     }
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8000);
 
         System.out.println("Socket na porta 8000");
@@ -87,19 +89,19 @@ public class Server {
                     outputStream.writeUTF(getAllLists() + menu());
                     break;
                 case 2:
-                    outputStream.writeUTF("Listas Existentes:" + getListsName() + "\nDigite qual lista");
+                    outputStream.writeUTF("Listas Existentes:\n" + getListsName() + "\nDigite qual lista");
 
-                    listName = inputStream.readUTF();
+                    listName = inputStream.readUTF().toLowerCase(Locale.ROOT);
 
                     outputStream.writeUTF("Digite o nome do elemento");
-                    element = inputStream.readUTF();
+                    element = inputStream.readUTF().toLowerCase(Locale.ROOT);
 
                     addElement(listName,element);
                     outputStream.writeUTF(menu());
                     break;
                 case 3:
                     outputStream.writeUTF("Digite o nome da lista");
-                    listName = inputStream.readUTF();
+                    listName = inputStream.readUTF().toLowerCase(Locale.ROOT);
 
                     lists.put(listName, new ArrayList<String>());
                     outputStream.writeUTF(menu());
@@ -109,10 +111,10 @@ public class Server {
                     break;
                 case 5:
                     outputStream.writeUTF("Digite a lista (existente) a que o elemento pertence");
-                    listName = inputStream.readUTF();
+                    listName = inputStream.readUTF().toLowerCase(Locale.ROOT);
 
                     outputStream.writeUTF("Digite o elemento que deseja remover");
-                    element = inputStream.readUTF();
+                    element = inputStream.readUTF().toLowerCase(Locale.ROOT);
 
                     removeElement(listName,element);
                     outputStream.writeUTF(menu());
@@ -135,8 +137,8 @@ public class Server {
 
             message = inputStream.readUTF();
             opt = Integer.parseInt(message);
-            listName = null;
-            element = null;
+            //listName = null;
+            //element = null;
         }
     }
 
